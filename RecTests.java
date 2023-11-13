65e442cdc9cfc7cef6b524233be59fb1a4b80a2b
MoNam97 <mohamad0namdar@gmail.com>
2023-11-13 16:44:52 +0330

    add test for get & set of rectangle

--- RecTests.java
+++ RecTests.java
@@ -6,4 +6,14 @@ public class RecTests{
         Rectangle r = new Rectangle(2, 5);
         Assert.assertEquals(10, r.computeArea());
     }
+    @Test
+    public void testRecParameterChange() {
+        Rectangle r = new Rectangle(4, 6);
+        Assert.assertEquals(4, r.getWidth());
+        Assert.assertEquals(6, r.getHeight());
+        r.setWidth(8);
+        r.setHeight(10);
+        Assert.assertEquals(8, r.getWidth());
+        Assert.assertEquals(10, r.getHeight());
+    }
 }
