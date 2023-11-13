9b39e45479bc18af68660bc785aed91b401439ae
MoNam97 <mohamad0namdar@gmail.com>
2023-11-13 16:47:26 +0330

    add get & set method to rectangle class and pass tests

--- Rectangle.java
+++ Rectangle.java
@@ -4,10 +4,27 @@ public class Rectangle {
     private int height;
 
     public Rectangle(int width, int height) {
-        this.width = width; this.height = height;
+        this.width = width; 
+        this.height = height;
     }
 
     public int computeArea() {
         return width * height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int w) {
+        width = w;
+    }
+
+    public void setHeight(int h) {
+        height = h;
     }
 }
